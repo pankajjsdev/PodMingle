cd6a8b2b38a0ef06b2eece00e372988f2a919a10
pankajjsdev <pankaj.js.dev@gmail.com>
2024-07-26 18:04:29 +0530

    add terms & policy pages

---
+++ app/(root)/privacy-policy/page.tsx
@@ -0,0 +1,35 @@
+import Section from "@/components/common/Section"
+import { H1, H4, P } from "@/components/common/Typography"
+
+function page() {
+    return (
+        <main>
+            <Section>
+                <div className="container space-y-12">
+                    <div className="space-y-4 mb-20 text-center">
+                        <H1>Privacy Policy</H1>
+                        <P>Lorem ipsum dolor sit amet consectetur adipiscing elit sed do eiusmod tempor incididunt ut labore et dolore.</P>
+                    </div>
+                     <div className="space-y-4">
+                        <H4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</H4>
+                        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe delectus cum nostrum earum deleniti architecto nulla molestiae esse nisi ex nihil nobis, doloremque, blanditiis fuga fugit, sapiente dolores iste rem.</P>
+                        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe delectus cum nostrum earum deleniti architecto nulla molestiae esse nisi ex nihil nobis, doloremque, blanditiis fuga fugit, sapiente dolores iste rem.</P>
+
+                    </div>
+                     <div className="space-y-4">
+                        <H4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</H4>
+                        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe delectus cum nostrum earum deleniti architecto nulla molestiae esse nisi ex nihil nobis, doloremque, blanditiis fuga fugit, sapiente dolores iste rem.</P>
+
+                    </div>
+                     <div className="space-y-4">
+                        <H4>Lorem ipsum dolor sit amet, consectetur adipiscing elit</H4>
+                        <P>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Saepe delectus cum nostrum earum deleniti architecto nulla molestiae esse nisi ex nihil nobis, doloremque, blanditiis fuga fugit, sapiente dolores iste rem.</P>
+
+                    </div>
+                </div>
+            </Section>
+        </main>
+    )
+}
+
+export default page
